d4c981d6bc3798411dce1f77f3e42f851accd0b7
Vinicius Pecorari <ovinicius.pecorari@gmail.com>
2024-04-17 13:13:28 -0300

    implementando consultar saldo

--- Controllers/WalletController.cs
+++ Controllers/WalletController.cs
@@ -4,9 +4,11 @@ using Microsoft.AspNetCore.Mvc;
 using SimplifiedBankingApi.Contracts;
 using SimplifiedBankingApi.Models.Dto;
 using SimplifiedBankingApi.Repository;
+using System.Runtime.InteropServices;
 
 namespace SimplifiedBankingApi.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class WalletController : ControllerBase
@@ -20,15 +22,25 @@ namespace SimplifiedBankingApi.Controllers
             _authRepository = authRepository;
         }
 
-        [Authorize] 
+        
         [HttpPut]
-        [Route("/api/deposit")]
+        [Route("/api/depositing")]
         public async Task<IActionResult> Deposit(WalletUpdatedDto walletUpdate)
         {
             var UserAuthWalletId = await _authRepository.GetUserIdToken(); // Recupera walletId do usuário logado
             await _walletRepository.Deposit(UserAuthWalletId.Id, walletUpdate.WalletBalance);
 
             return Ok();
+        }
+
+        [HttpGet]
+        [Route("/api/check-balance")]
+        public async Task<ActionResult<WalletUpdatedDto>> GetBalance()
+        {
+            var UserAuthWalletId = await _authRepository.GetUserIdToken(); // Recupera walletId do usuário logado
+            var balance = await _walletRepository.GetBalance(UserAuthWalletId.Id);
+
+            return balance;
         }
     }
 }
